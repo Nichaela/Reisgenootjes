78824c3267c85214489c574372aa19c668b8721f
Nichaela <NichaelaIrina@outlook.com>
2026-02-19 13:17:19 +0100

    css setup

--- server.js
+++ server.js
@@ -132,6 +132,9 @@ async function start() {
 
     // Middleware to handle not found errors - error 404
     app.use((req, res) => {
+      if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') {
+        return res.sendStatus(204)
+      }
       console.error('404 error at URL: ' + req.url)
       res.status(404).send('404 error at URL: ' + req.url)
     })
