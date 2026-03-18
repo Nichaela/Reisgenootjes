14d9d918d27676da3056b902cdacefa5e4fd7bcc
Annabellovesyou <Annabel.claij@hva.nl>
2026-03-18 17:48:28 +0100

    wijziging: ontdekfilter route toegevoegd dus filter doet het weer

--- server.js
+++ server.js
@@ -185,14 +185,33 @@ function registerGetRoutes() {
   // route naar ontdek filter
   app.get('/ontdekfilter', async (req, res) => {
     try {
-      const myUsers = await users.find({}).toArray()
-      res.render('pages/ontdekfilter', { users: myUsers })
+      const db = client.db(process.env.DB_NAME);
+      const usersCollection = db.collection('users');
+      const discoverCollection = db.collection('discover');
+      const reizen = await discoverCollection.find({}).toArray();
+      const resultaat = []; for (const reis of reizen) {
 
-    } catch (err) {
-      console.error(err)
-      res.status(500).send("Fout bij ophalen data")
-    }
+        //voor elke reis in de lijst reizen doe dit: 
+        const user = await usersCollection.findOne({
+          _id: reis.userId //vind een reis 
+        })
+        resultaat.push({ //pusht deze data in die lege array genaamd resultaat 
+          reis: reis, user: user
+        })
+      }
+
+      res.render('pages/ontdekfilter', {
+        reizen: resultaat //reizen = de array van de collection en resultaat is de array die ik heb gemaakt 
+      })
+    } catch (err) { console.error(err); res.status(500).send("Fout bij ophalen data"); }
   })
+
+  app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+      res.redirect('/welkom')
+    })
+  })
+}
 
   //Huidge route naar filter menu + werkende continent filter 
   app.get('/filter', async (req, res) => {
@@ -223,7 +242,7 @@ function registerGetRoutes() {
       res.redirect('/welkom')
     })
   })
-}
+
 
 // =======================
 // POST ROUTES
