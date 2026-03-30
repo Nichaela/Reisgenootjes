1a39fd5e855b2390810b67bce84c2b8b07570100
Annabellovesyou <Annabel.claij@hva.nl>
2026-03-30 16:53:39 +0200

    wijzigingen: CSS: niks veranderd in matchen.css SERVER: regel 289 toegevoegd in server zodat matchen reset weer werkt, set-preference route toegevoegd (backend filteren op matchen pagina), kleine stukjes verwijderd van genderPreference die niet meer nodig waren. JAVASCRIPT: filter.js aangevuld met code (is nu alleen in gebruik voor discover pagina want matchen pagina is backend en dus niet js), form.js aangepast (filter code eruit gehaald), ontdekfilter.js verwijderd want staat nu in filter.js, partial filtermenu weer verwijderd uit matchen.ejs. Tenslotte nog in discover.ejs script gelinkt naar filter.js en een paar haakjes verwijderd (;)

--- server.js
+++ server.js
@@ -233,7 +233,8 @@ app.get('/matchen', async (req, res) => {
 
     const mijnId = new ObjectId(req.session.user._id)
 
-    const voorkeur = req.session.genderPreference //toegevoegd door annabel
+
+    const voorkeur = req.session.genderPreference //er wordt gekeken of er een filter is geselecteerd
 
 const query = {
   _id: {
@@ -242,7 +243,7 @@ const query = {
   }
 }
 
-// alleen filteren als er een voorkeur is
+// als er voorkeur is opgegeven dan: voeg aan query de voorkeur toe
 if (voorkeur) {
   query.gender = voorkeur
 }
@@ -285,6 +286,7 @@ const matchUser = await users.findOne(query)
 
   app.get('/matchen/reset', (req, res) => {
   req.session.gezien = [];
+  req.session.genderPreference = null 
   res.redirect('/matchen')
 })
 
@@ -608,6 +610,11 @@ function registerPostRoutes() {
   return res.redirect('/discover')
 })
  
+//gender voorkeur bij matchen, realtime update
+app.post('/set-preference', (req, res) => {
+  req.session.genderPreference = req.body.genderPreference
+  res.redirect('/matchen')
+})
 
   // create-post formulier
   app.post('/post', upload.single('postCoverImg'), async (req, res) => {
@@ -713,16 +720,11 @@ function registerPostRoutes() {
   try {
     const matchedUserId = req.body.matchedUser
     const actie = req.body.actie
-    const genderPreference = req.body.genderPreference
 
     if (!req.session.gezien) req.session.gezien = []
 
     if (matchedUserId && !req.session.gezien.includes(matchedUserId)) {
       req.session.gezien.push(matchedUserId)
-    }
-
-    if (genderPreference) {
-      req.session.genderPreference = genderPreference
     }
 
     if (actie === 'like') {
